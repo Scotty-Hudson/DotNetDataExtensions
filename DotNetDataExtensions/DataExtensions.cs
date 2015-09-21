1e239a6b0db0eb8fd37396dcb5c0c200425f2a8e
shudson <scotty.hudson@crackerbarrel.com>
2015-09-21 00:12:54 -0500

    Intial Commit

---
+++ DotNetDataExtensions/DataExtensions.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Globalization;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Runtime.Serialization;
+using FastMember;
+
+// These extensions require the third party FastMember.dll that
+// uses IL instead of reflection for significant performance gains.
+// Use the NuGet package.
+namespace DataExtensions
+{
+    // Shamelessly stolen from StackOverflow titled:
+    // Fast creation of objects instead of Activator.CreateInstance(type)
+    internal static class New<T>
+    {
+        internal static readonly Func<T> Instance = Creator();
+
+        private static Func<T> Creator()
+        {
+            var t = typeof(T);
+            if (t == typeof(string))
+                return Expression.Lambda<Func<T>>(Expression.Constant(string.Empty)).Compile();
+
+            if (t.HasDefaultConstructor())
+                return Expression.Lambda<Func<T>>(Expression.New(t)).Compile();
+
+            return () => (T)FormatterServices.GetUninitializedObject(t);
+        }
+    }
+
+    public static class DataExtensionMethods
+    {
+        public static bool HasDefaultConstructor(this Type t)
+        {
+            return t.IsValueType || t.GetConstructor(Type.EmptyTypes) != null;
+        }
+
+        public static T ConvertTo<T>(this DataRow row, string columnName)
+        {
+            return (T)Convert.ChangeType(row[columnName], typeof(T), CultureInfo.CurrentCulture);
+        }
+
+        public static T ConvertTo<T>(this DataRow row, string columnName, T defaultValue)
+        {
+            if (row[columnName] is DBNull) return defaultValue;
+            return row.ConvertTo<T>(columnName);
+        }
+
+        public static T MapTo<T>(this DataRow row) where T : class
+        {
+            var entity = New<T>.Instance();
+            return row.MapTo(entity);
+        }
+
+        public static T MapTo<T>(this DataRow row, T defaultEntity) where T : class
+        {
+            // Third party from nuget - faster than reflection
+            var accessor = TypeAccessor.Create(typeof(T));
+
+            foreach (var prop in accessor.GetMembers())
+            {
+                // We only want to map to properties in our entity with non null values
+                // and return the existing object values for the rest.
+                if (!row.Table.Columns.Contains(prop.Name) || row[prop.Name] is DBNull)
+                {
+                    // Most of the time, I prefer empty strings over nulls
+                    if (prop.Type == typeof(string))
+                        accessor[defaultEntity, prop.Name] = string.Empty;
+                    continue;
+                }
+
+                // Nullable fields in the POCO should be null if the value in the DataRow is null
+                var t = Nullable.GetUnderlyingType(prop.Type) ?? prop.Type;
+                var safe = row.IsNull(prop.Name)
+                   ? null
+                    : Convert.ChangeType(row[prop.Name], t, CultureInfo.CurrentCulture);
+                accessor[defaultEntity, prop.Name] = safe;
+            }
+            return defaultEntity;
+        }
+
+        public static List<T> MapTo<T>(this DataTable table) where T : class
+        {
+            var entityList = new List<T>();
+            entityList.AddRange(table.Rows.Cast<DataRow>()
+                                     .Select(row => new { row, entity = New<T>.Instance() })
+                                     .Select(@t => MapTo(@t.row, @t.entity)));
+            return entityList;
+        }
+
+        public static List<T> MapTo<T>(this DataTable table, T defaultEntity) where T : class
+        {
+            // Third party from nuget - faster than reflection
+            var accessor = TypeAccessor.Create(typeof(T));
+
+            var entityList = new List<T>();
+            foreach (DataRow row in table.Rows)
+            {
+                var entity = New<T>.Instance();
+                foreach (var prop in accessor.GetMembers())
+                    accessor[entity, prop.Name] = accessor[defaultEntity, prop.Name];
+
+                entityList.Add(row.MapTo(entity));
+            }
+            return entityList;
+        }
+
+        public static List<T> MapTo<T>(this IDataReader reader) where T : class
+        {
+            var entity = New<T>.Instance();
+            return reader.MapTo(entity);
+        }
+
+        public static List<T> MapTo<T>(this IDataReader reader, T defaultEntity) where T : class
+        {
+            // Third party from nuget - faster than reflection
+            var accessor = TypeAccessor.Create(typeof(T));
+
+            // Cache the field names in the reader for efficiency
+            var readerFieldLookup = new Dictionary<string, int>(StringComparer.OrdinalIgnoreCase); // store name and ordinal
+            for (var i = 0; i < reader.FieldCount; i++)
+                readerFieldLookup.Add(reader.GetName(i), i);
+
+            // Ignore extra fields in the DataReader and ignore extra properties in our POCO.
+            var entityList = new List<T>();
+            while (reader.Read())
+            {
+                var entity = New<T>.Instance();
+                foreach (var prop in accessor.GetMembers())
+                {
+                    // Set our new object of type T's value for this property to the value of the instance passed in.
+                    accessor[entity, prop.Name] = accessor[defaultEntity, prop.Name];
+
+                    // We only want to map to properties in our entity with non null values
+                    // and return the existing object's values for the rest.
+                    if (!readerFieldLookup.ContainsKey(prop.Name) || reader[prop.Name] is DBNull)
+                    {
+                        // Most of the time, I prefer empty strings over nulls
+                        if (prop.Type == typeof(string))
+                            accessor[defaultEntity, prop.Name] = string.Empty;
+
+                        continue;
+                    }
+
+                    // Nullable fields in the POCO should be null if the value in the DataRow is null
+                    var t = Nullable.GetUnderlyingType(prop.Type) ?? prop.Type;
+                    var safe = reader.IsDBNull(readerFieldLookup[prop.Name])
+                        ? null
+                        : Convert.ChangeType(reader.GetValue(readerFieldLookup[prop.Name]), t, CultureInfo.CurrentCulture);
+                    accessor[entity, prop.Name] = safe;
+                }
+                entityList.Add(entity);
+            }
+            return entityList;
+        }
+    }
+}
---
+++ DotNetDataExtensionsTests/UnitTests.cs
@@ -0,0 +1,241 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using NUnit.Framework;
+using DataExtensions;
+
+namespace DotNetDataExtensionsTests
+{ 
+    public class UnitTests
+    {
+        private DataTable _dtTest;
+        
+        public UnitTests()
+        {
+            _dtTest = CreateDataTable();            
+        }
+
+        [TestCase]
+        public void DataReader_ReturnsCorrectListCount()
+        {
+            using (var reader = _dtTest.CreateDataReader())
+            {
+                var customerList = reader.MapTo<Customer>();
+                Assert.That(customerList.Count(), Is.EqualTo(3));
+            }
+        }
+
+        [TestCase]
+        public void DataTable_ReturnsCorrectListCount()
+        {
+            var customerList = _dtTest.MapTo<Customer>();
+            Assert.That(customerList.Count(), Is.EqualTo(3));
+        }
+
+        [TestCase]
+        public void DataReader_AreFieldsCorrectInFirstRecord()
+        {
+            using (var reader = _dtTest.CreateDataReader())
+            {
+                var customerList = reader.MapTo<Customer>();
+                var cust = customerList.First(c => c.CustomerId == 1);
+                Assert.That(cust.FirstName, Is.EqualTo("John"));
+                Assert.That(cust.LastName, Is.EqualTo("Doe"));
+                Assert.That(cust.Email, Is.EqualTo("johnDoe@maxmail.com"));
+                Assert.That(cust.PhoneNumber, Is.EqualTo("345-231-9234"));
+                Assert.That(cust.Address, Is.EqualTo("312 Brackish Rd"));
+                Assert.That(cust.City, Is.EqualTo("Boston"));
+                Assert.That(cust.State, Is.EqualTo("MA"));
+                Assert.That(cust.Zip, Is.EqualTo(34567));
+                Assert.That(cust.RewardsPoints, Is.EqualTo(23.3M));
+            }
+        }
+
+        [TestCase]
+        public void DataTable_AreFieldsCorrectInFirstRecord()
+        {
+            var customerList = _dtTest.MapTo<Customer>();
+            var cust = customerList.First(c => c.CustomerId == 1);
+            Assert.That(cust.FirstName, Is.EqualTo("John"));
+            Assert.That(cust.LastName, Is.EqualTo("Doe"));
+            Assert.That(cust.Email, Is.EqualTo("johnDoe@maxmail.com"));
+            Assert.That(cust.PhoneNumber, Is.EqualTo("345-231-9234"));
+            Assert.That(cust.Address, Is.EqualTo("312 Brackish Rd"));
+            Assert.That(cust.City, Is.EqualTo("Boston"));
+            Assert.That(cust.State, Is.EqualTo("MA"));
+            Assert.That(cust.Zip, Is.EqualTo(34567));
+            Assert.That(cust.RewardsPoints, Is.EqualTo(23.3M));
+        }
+
+        [TestCase]
+        public void DataRow_AreFieldsCorrectInFirstRecord()
+        {
+            var dr = _dtTest.Select("CustomerId = 1");
+            var cust = dr[0].MapTo<Customer>();
+            Assert.That(cust.FirstName, Is.EqualTo("John"));
+            Assert.That(cust.LastName, Is.EqualTo("Doe"));
+            Assert.That(cust.Email, Is.EqualTo("johnDoe@maxmail.com"));
+            Assert.That(cust.PhoneNumber, Is.EqualTo("345-231-9234"));
+            Assert.That(cust.Address, Is.EqualTo("312 Brackish Rd"));
+            Assert.That(cust.City, Is.EqualTo("Boston"));
+            Assert.That(cust.State, Is.EqualTo("MA"));
+            Assert.That(cust.Zip, Is.EqualTo(34567));
+            Assert.That(cust.RewardsPoints, Is.EqualTo(23.3M));
+        }
+
+        [TestCase]
+        public void DataReader_DoesNullableTypeSetNullCorrectly()
+        {
+            using (var reader = _dtTest.CreateDataReader())
+            {
+                var customerList = reader.MapTo<Customer>();
+                var cust = customerList.First(c => c.CustomerId == 2);
+                Assert.That(cust.RewardsPoints, Is.EqualTo(null));
+            }
+        }
+
+        [TestCase]
+        public void DataTable_DoesNullableTypeSetNullCorrectly()
+        {
+            var customerList = _dtTest.MapTo<Customer>();
+            var cust = customerList.First(c => c.CustomerId == 2);
+            Assert.That(cust.RewardsPoints, Is.EqualTo(null));
+        }
+
+        [TestCase]
+        public void DataRow_DoesNullableTypeSetNullCorrectly()
+        {
+            var dr = _dtTest.Select("CustomerId = 2");
+            var cust = dr[0].MapTo<Customer>();
+            Assert.That(cust.RewardsPoints, Is.EqualTo(null));
+        }
+
+        [TestCase]
+        public void DataReader_DoesNonNullableTypeSetDefaultCorrectly()
+        {
+            using (var reader = _dtTest.CreateDataReader())
+            {
+                var customerList = reader.MapTo<Customer>();
+                var cust = customerList.First(c => c.CustomerId == 2);
+                Assert.That(cust.Zip, Is.EqualTo(0));
+            }
+        }
+
+        [TestCase]
+        public void DataTable_DoesNonNullableTypeSetDefaultCorrectly()
+        {
+            var customerList = _dtTest.MapTo<Customer>();
+            var cust = customerList.First(c => c.CustomerId == 2);
+            Assert.That(cust.Zip, Is.EqualTo(0));
+        }
+
+        [TestCase]
+        public void DataRow_DoesNonNullableTypeSetDefaultCorrectly()
+        {
+            var dr = _dtTest.Select("CustomerId = 2");
+            var cust = dr[0].MapTo<Customer>();
+            Assert.That(cust.Zip, Is.EqualTo(0));
+        }
+
+        [TestCase]
+        public void DataReader_DoesDBNullOverrideCorrectly()
+        {
+            using (var reader = _dtTest.CreateDataReader())
+            {
+                var customerList = reader.MapTo(new Customer { Zip = 0 });
+                var cust = customerList.First(c => c.CustomerId == 2);
+                Assert.That(cust.Zip, Is.EqualTo(0));
+            }
+        }
+
+        [TestCase]
+        public void DataTable_DoesDBNullOverrideCorrectly()
+        {
+            var customerList = _dtTest.MapTo(new Customer { Zip = 0 });
+            var cust = customerList.First(c => c.CustomerId == 2);
+            Assert.That(cust.Zip, Is.EqualTo(0));
+        }
+
+        [TestCase]
+        public void DataRow_DoesDBNullOverrideCorrectly()
+        {
+            var dr = _dtTest.Select("CustomerId = 2");
+            var cust = dr[0].MapTo(new Customer { Zip = 0 });
+            Assert.That(cust.Zip, Is.EqualTo(0));
+        }
+
+        private static DataTable CreateDataTable()
+        {
+            var c = new Customer();
+            var dt = new DataTable();
+
+            dt.Columns.Add(nameof(c.CustomerId), typeof(long));
+            dt.Columns.Add(nameof(c.FirstName), typeof(string));
+            dt.Columns.Add(nameof(c.LastName), typeof(string));
+            dt.Columns.Add(nameof(c.Email), typeof(string));
+            dt.Columns.Add(nameof(c.PhoneNumber), typeof(string));
+            dt.Columns.Add(nameof(c.Address), typeof(string));
+            dt.Columns.Add(nameof(c.City), typeof(string));
+            dt.Columns.Add(nameof(c.State), typeof(string));
+            dt.Columns.Add(nameof(c.Zip), typeof(int));
+            dt.Columns.Add(nameof(c.RewardsPoints), typeof(decimal));
+
+            var dr = dt.NewRow();
+            dr[nameof(c.CustomerId)] = 1;
+            dr[nameof(c.FirstName)] = "John";
+            dr[nameof(c.LastName)] = "Doe";
+            dr[nameof(c.Email)] = "johnDoe@maxmail.com";
+            dr[nameof(c.PhoneNumber)] = "345-231-9234";
+            dr[nameof(c.Address)] = "312 Brackish Rd";
+            dr[nameof(c.City)] = "Boston";
+            dr[nameof(c.State)] = "MA";
+            dr[nameof(c.Zip)] = "34567";
+            dr[nameof(c.RewardsPoints)] = 23.3M;
+            dt.Rows.Add(dr);
+
+            dr = dt.NewRow();
+            dr[nameof(c.CustomerId)] = 2;
+            dr[nameof(c.FirstName)] = "Jake";
+            dr[nameof(c.LastName)] = "McPhelson";
+            dr[nameof(c.Email)] = "Jake123@mail.com";
+            dr[nameof(c.PhoneNumber)] = DBNull.Value;
+            dr[nameof(c.Address)] = "64 Back Road Drive";
+            dr[nameof(c.City)] = "Houston";
+            dr[nameof(c.State)] = "TX";
+            dr[nameof(c.Zip)] = DBNull.Value;
+            dr[nameof(c.RewardsPoints)] = DBNull.Value;
+            dt.Rows.Add(dr);
+
+            dr = dt.NewRow();
+            dr[nameof(c.CustomerId)] = 3;
+            dr[nameof(c.FirstName)] = "Bob";
+            dr[nameof(c.LastName)] = "Jackson";
+            dr[nameof(c.Email)] = "Jake123@vixmix.com";
+            dr[nameof(c.PhoneNumber)] = DBNull.Value;
+            dr[nameof(c.Address)] = "2345 Cumberland St.";
+            dr[nameof(c.City)] = "Nashville";
+            dr[nameof(c.State)] = "TN";
+            dr[nameof(c.Zip)] = 37210;
+            dr[nameof(c.RewardsPoints)] = 0M;
+            dt.Rows.Add(dr);
+            return dt;
+        }
+    }
+
+    public class Customer
+    {
+        public long CustomerId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public int Zip { get; set; }
+        public decimal? RewardsPoints { get; set; }
+    }       
+}
