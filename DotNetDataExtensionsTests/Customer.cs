496d24c24d86621547b714a3599d9a9be2ba9329
shudson <scotty.hudson@crackerbarrel.com>
2015-12-24 00:40:19 -0600

    Added unit tests for ConvertTo. Refactored unit
    test. The unit tests now get the test data from an xml file.

---
+++ DotNetDataExtensionsTests/Customer.cs
@@ -0,0 +1,16 @@
+﻿namespace DotNetDataExtensionsTests
+{
+    public class Customer
+    {
+        public long Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public int Zip { get; set; }
+        public decimal? RewardsPoints { get; set; }
+    }
+}
--- DotNetDataExtensionsTests/UnitTests.cs
+++ DotNetDataExtensionsTests/UnitTests.cs
@@ -9,6 +9,7 @@ namespace DotNetDataExtensionsTests
     public class UnitTests
     {
         private readonly DataTable _dtTest;
+        private readonly Customer _customer = new Customer(); // Here only for 'nameof' operator use
 
         public UnitTests()
         {
@@ -16,18 +17,24 @@ namespace DotNetDataExtensionsTests
             _dtTest.ReadXml(@"..\..\TestData.xml");
         }
 
-        private void TestAllCustomerPropertiesById(Customer cust)
+        private void TestAllCustomerPropertiesById(Customer c)
         {
-            var dr = _dtTest.Select($"{nameof(cust.CustomerId)} = {cust.CustomerId}")[0];
-            Assert.That(cust.FirstName, Is.EqualTo(dr.ConvertTo<string>(nameof(cust.FirstName))));
-            Assert.That(cust.LastName, Is.EqualTo(dr.ConvertTo<string>(nameof(cust.LastName))));
-            Assert.That(cust.Email, Is.EqualTo(dr.ConvertTo<string>(nameof(cust.Email))));
-            Assert.That(cust.PhoneNumber, Is.EqualTo(dr.ConvertTo<string>(nameof(cust.PhoneNumber))));
-            Assert.That(cust.Address, Is.EqualTo(dr.ConvertTo<string>(nameof(cust.Address))));
-            Assert.That(cust.City, Is.EqualTo(dr.ConvertTo<string>(nameof(cust.City))));
-            Assert.That(cust.State, Is.EqualTo(dr.ConvertTo<string>(nameof(cust.State))));
-            Assert.That(cust.Zip, Is.EqualTo(dr.ConvertTo<int>(nameof(cust.Zip))));
-            Assert.That(cust.RewardsPoints, Is.EqualTo((Nullable<decimal>)dr[nameof(cust.RewardsPoints)]));
+            var dr = _dtTest.Select($"{nameof(c.Id)} = {c.Id}")[0];
+            TestAllCustomerPropertiesById(c, dr);
+        }
+
+        private void TestAllCustomerPropertiesById(Customer c, DataRow dr)
+        {
+            Assert.That(c.FirstName, Is.EqualTo(dr.ConvertTo<string>(nameof(c.FirstName))));
+            Assert.That(c.LastName, Is.EqualTo(dr.ConvertTo<string>(nameof(c.LastName))));
+            Assert.That(c.Email, Is.EqualTo(dr.ConvertTo<string>(nameof(c.Email))));
+            Assert.That(c.PhoneNumber, Is.EqualTo(dr.ConvertTo<string>(nameof(c.PhoneNumber))));
+            Assert.That(c.Address, Is.EqualTo(dr.ConvertTo<string>(nameof(c.Address))));
+            Assert.That(c.City, Is.EqualTo(dr.ConvertTo<string>(nameof(c.City))));
+            Assert.That(c.State, Is.EqualTo(dr.ConvertTo<string>(nameof(c.State))));
+            Assert.That(c.Zip, Is.EqualTo(dr.ConvertTo(nameof(c.Zip), 0)));
+            Assert.That(c.RewardsPoints, Is.EqualTo(dr[nameof(c.RewardsPoints)]
+                == DBNull.Value ? null : dr[nameof(c.RewardsPoints)]));
         }
 
         [TestCase]
@@ -36,7 +43,7 @@ namespace DotNetDataExtensionsTests
             using (var reader = _dtTest.CreateDataReader())
             {
                 var customerList = reader.MapTo<Customer>();
-                Assert.That(customerList.Count, Is.EqualTo(3));
+                Assert.That(customerList.Count, Is.EqualTo(_dtTest.Rows.Count));
             }
         }
 
@@ -44,34 +51,39 @@ namespace DotNetDataExtensionsTests
         public void DataTable_ReturnsCorrectListCount()
         {
             var customerList = _dtTest.MapTo<Customer>();
-            Assert.That(customerList.Count, Is.EqualTo(3));
+            Assert.That(customerList.Count, Is.EqualTo(_dtTest.Rows.Count));
         }
 
         [TestCase]
-        public void DataReader_AreFieldsCorrectInFirstRecord()
+        public void DataReader_AreAllPropertiesMappedCorrectFromAllRows()
         {
             using (var reader = _dtTest.CreateDataReader())
             {
                 var customerList = reader.MapTo<Customer>();
-                var cust = customerList.First(c => c.CustomerId == 1);
-                TestAllCustomerPropertiesById(cust);
+                foreach (var c in customerList)
+                {
+                    TestAllCustomerPropertiesById(c);
+                }
             }
-        }
-        
-        [TestCase]
-        public void DataTable_AreFieldsCorrectInFirstRecord()
-        {
-            var customerList = _dtTest.MapTo<Customer>();
-            var cust = customerList.First(c => c.CustomerId == 1);
-            TestAllCustomerPropertiesById(cust);
         }
 
         [TestCase]
-        public void DataRow_AreFieldsCorrectInFirstRecord()
+        public void DataTable_AreAllPropertiesMappedCorrectFromAllRows()
         {
-            var dr = _dtTest.Select("CustomerId = 1");
-            var cust = dr[0].MapTo<Customer>();
-            TestAllCustomerPropertiesById(cust);
+            var customerList = _dtTest.MapTo<Customer>();
+            foreach (var c in customerList)
+            {
+                 TestAllCustomerPropertiesById(c);
+            }
+        }
+
+        [TestCase]
+        public void DataRow_AreAllPropertiesMappedCorrectFromAllRows()
+        {
+            foreach (DataRow dr in _dtTest.Rows)
+            {
+                TestAllCustomerPropertiesById(dr.MapTo<Customer>(), dr);
+            }
         }
 
         [TestCase]
@@ -80,7 +92,7 @@ namespace DotNetDataExtensionsTests
             using (var reader = _dtTest.CreateDataReader())
             {
                 var customerList = reader.MapTo<Customer>();
-                var cust = customerList.First(c => c.CustomerId == 2);
+                var cust = customerList.First(c => c.Id == 2);
                 Assert.IsNull(cust.RewardsPoints);
             }
         }
@@ -89,14 +101,14 @@ namespace DotNetDataExtensionsTests
         public void DataTable_DoesNullableTypeSetNullCorrectly()
         {
             var customerList = _dtTest.MapTo<Customer>();
-            var cust = customerList.First(c => c.CustomerId == 2);
+            var cust = customerList.First(c => c.Id == 2);
             Assert.IsNull(cust.RewardsPoints);
         }
 
         [TestCase]
         public void DataRow_DoesNullableTypeSetNullCorrectly()
         {
-            var dr = _dtTest.Select("CustomerId = 2");
+            var dr = _dtTest.Select($"{nameof(_customer.Id)} = 2");
             var cust = dr[0].MapTo<Customer>();
             Assert.IsNull(cust.RewardsPoints);
         }
@@ -107,7 +119,7 @@ namespace DotNetDataExtensionsTests
             using (var reader = _dtTest.CreateDataReader())
             {
                 var customerList = reader.MapTo<Customer>();
-                var cust = customerList.First(c => c.CustomerId == 2);
+                var cust = customerList.First(c => c.Id == 2);
                 Assert.That(cust.Zip, Is.EqualTo(0));
             }
         }
@@ -116,14 +128,14 @@ namespace DotNetDataExtensionsTests
         public void DataTable_DoesNonNullableTypeSetDefaultCorrectly()
         {
             var customerList = _dtTest.MapTo<Customer>();
-            var cust = customerList.First(c => c.CustomerId == 2);
+            var cust = customerList.First(c => c.Id == 2);
             Assert.That(cust.Zip, Is.EqualTo(0));
         }
 
         [TestCase]
         public void DataRow_DoesNonNullableTypeSetDefaultCorrectly()
         {
-            var dr = _dtTest.Select("CustomerId = 2");
+            var dr = _dtTest.Select($"{nameof(_customer.Id)} = 2");
             var cust = dr[0].MapTo<Customer>();
             Assert.That(cust.Zip, Is.EqualTo(0));
         }
@@ -134,7 +146,7 @@ namespace DotNetDataExtensionsTests
             using (var reader = _dtTest.CreateDataReader())
             {
                 var customerList = reader.MapTo(new Customer { Zip = 0 });
-                var cust = customerList.First(c => c.CustomerId == 2);
+                var cust = customerList.First(c => c.Id == 2);
                 Assert.That(cust.Zip, Is.EqualTo(0));
             }
         }
@@ -143,14 +155,14 @@ namespace DotNetDataExtensionsTests
         public void DataTable_DoesDBNullOverrideCorrectly()
         {
             var customerList = _dtTest.MapTo(new Customer { Zip = 0 });
-            var cust = customerList.First(c => c.CustomerId == 2);
+            var cust = customerList.First(c => c.Id == 2);
             Assert.That(cust.Zip, Is.EqualTo(0));
         }
 
         [TestCase]
         public void DataRow_DoesDBNullOverrideCorrectly()
         {
-            var dr = _dtTest.Select("CustomerId = 2");
+            var dr = _dtTest.Select($"{nameof(_customer.Id)} = 2");
             var cust = dr[0].MapTo(new Customer { Zip = 0 });
             Assert.That(cust.Zip, Is.EqualTo(0));
         }
@@ -161,7 +173,7 @@ namespace DotNetDataExtensionsTests
             using (var reader = _dtTest.CreateDataReader())
             {
                 var customerList = reader.MapTo<Customer>();
-                var cust = customerList.First(c => c.CustomerId == 3);
+                var cust = customerList.First(c => c.Id == 3);
                 Assert.That(cust.PhoneNumber, Is.EqualTo(string.Empty));
             }
         }
@@ -170,14 +182,14 @@ namespace DotNetDataExtensionsTests
         public void DataTable_DoesDBNullReturnCorrectlyForString()
         {
             var customerList = _dtTest.MapTo<Customer>();
-            var cust = customerList.First(c => c.CustomerId == 3);
+            var cust = customerList.First(c => c.Id == 3);
             Assert.That(cust.PhoneNumber, Is.EqualTo(string.Empty));
         }
 
         [TestCase]
-        public void DataRow_DoesDBNullReturnCorrectlyString()
+        public void DataRow_DoesDBNullReturnCorrectlyForString()
         {
-            var dr = _dtTest.Select("CustomerId = 3");
+            var dr = _dtTest.Select($"{nameof(_customer.Id)} = 3");
             var cust = dr[0].MapTo<Customer>();
             Assert.That(cust.PhoneNumber, Is.EqualTo(string.Empty));
         }
@@ -188,7 +200,7 @@ namespace DotNetDataExtensionsTests
             using (var reader = _dtTest.CreateDataReader())
             {
                 var customerList = reader.MapTo<Customer>(false);
-                var cust = customerList.First(c => c.CustomerId == 3);
+                var cust = customerList.First(c => c.Id == 3);
                 Assert.IsNull(cust.PhoneNumber);
             }
         }
@@ -197,30 +209,40 @@ namespace DotNetDataExtensionsTests
         public void DataTable_DoesDBNullReturnCorrectlyForStringWithNullOverrideTurnedOff()
         {
             var customerList = _dtTest.MapTo<Customer>(false);
-            var cust = customerList.First(c => c.CustomerId == 3);
+            var cust = customerList.First(c => c.Id == 3);
             Assert.IsNull(cust.PhoneNumber);
         }
 
         [TestCase]
         public void DataRow_DoesDBNullReturnCorrectlyForStringWithNullOverrideTurnedOff()
         {
-            var dr = _dtTest.Select("CustomerId = 3");
+            var dr = _dtTest.Select($"{nameof(_customer.Id)} = 3");
             var cust = dr[0].MapTo<Customer>(false);
             Assert.IsNull(cust.PhoneNumber);
         }
-    }
 
-    public class Customer
-    {
-        public long CustomerId { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public string PhoneNumber { get; set; }
-        public string Email { get; set; }
-        public string Address { get; set; }
-        public string City { get; set; }
-        public string State { get; set; }
-        public int Zip { get; set; }
-        public decimal? RewardsPoints { get; set; }
+        [TestCase]
+        public void ConvertTo_DoesReturnEmptyStringForDBNull()
+        {
+            var dr = _dtTest.Select($"{nameof(_customer.Id)} = 3");
+            var phone = dr[0].ConvertTo<string>(nameof(_customer.PhoneNumber));
+            Assert.That(phone, Is.EqualTo(string.Empty));
+        }
+
+        [TestCase]
+        public void ConvertTo_DoesNullValueOverrideCorrectly()
+        {
+            var dr = _dtTest.Select($"{nameof(_customer.Id)} = 2");
+            var zip = dr[0].ConvertTo(nameof(_customer.Zip), 11111);
+            Assert.That(zip, Is.EqualTo(11111));
+        }
+
+        [TestCase]
+        public void ConvertTo_DoesFieldWithValueMapCorrectly()
+        {
+            var dr = _dtTest.Select($"{nameof(_customer.Id)} = 1");
+            var firstName = dr[0].ConvertTo<string>(nameof(_customer.FirstName));
+            Assert.That(firstName, Is.EqualTo(dr[0][nameof(_customer.FirstName)]));
+        }
     }
 }
